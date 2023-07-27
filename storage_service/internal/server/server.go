9a396d21a60e70f80ea3b4df5158255292398aea
paw1a <paw1a@yandex.ru>
2023-07-27 22:24:09 +0300

    add media uploading

--- storage_service/internal/server/server.go
+++ storage_service/internal/server/server.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"context"
 	"github.com/minio/minio-go/v7"
 	"github.com/paw1a/grpc-media-converter/storage_service/config"
 	minio2 "github.com/paw1a/grpc-media-converter/storage_service/pkg/minio"
 	"github.com/pkg/errors"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Server struct {
@@ -18,11 +22,15 @@ func NewServer(cfg *config.Config) *Server {
 }
 
 func (s *Server) Run() error {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
 	minioClient, err := minio2.NewMinioClient(s.cfg.Minio)
 	if err != nil {
 		return errors.Wrap(err, "NewMinioClient")
 	}
 	log.Printf("minio connected: %v\n", minioClient.EndpointURL())
+	s.minioClient = minioClient
 
 	serverCloseFunc, grpcServer, err := s.newGrpcServer()
 	if err != nil {
@@ -30,6 +38,7 @@ func (s *Server) Run() error {
 	}
 	defer serverCloseFunc()
 
+	<-ctx.Done()
 	grpcServer.GracefulStop()
 
 	return nil
--- storage_service/internal/service/service.go
+++ storage_service/internal/service/service.go
@@ -2,11 +2,15 @@ package service
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 	"github.com/paw1a/grpc-media-converter/storage_service/config"
 	"github.com/paw1a/grpc-media-converter/storage_service/pb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
+	"log"
+	"os"
 )
 
 type GrpcService struct {
@@ -20,6 +24,35 @@ func NewGrpcService(cfg *config.Config, minioClient *minio.Client) *GrpcService 
 }
 
 func (s *GrpcService) UploadFile(stream pb.StorageService_UploadFileServer) error {
+	file, err := os.CreateTemp(os.TempDir(), "minio_temp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	for {
+		bytes, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return s.errResponse(codes.Internal, err)
+		}
+
+		data := bytes.GetBinary()
+		if _, err = file.Write(data); err != nil {
+			return s.errResponse(codes.Internal, err)
+		}
+	}
+	file.Close()
+
+	_, err = s.minioClient.FPutObject(context.Background(), s.cfg.Minio.BucketName,
+		uuid.New().String(), file.Name(), minio.PutObjectOptions{})
+	if err != nil {
+		return s.errResponse(codes.Internal, err)
+	}
+
 	return nil
 }
 
